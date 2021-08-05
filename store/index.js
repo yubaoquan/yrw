1d63cad04c513b91595265fd4621920b1878da6e
chenfu <chenfu@tsign.cn>
2021-08-05 16:55:55 +0800

    fix: npe

--- store/index.js
+++ store/index.js
@@ -4,6 +4,7 @@ const actions = {
   nuxtServerInit({ commit }, { req }) {
     if (req.headers.cookie) {
       const parsed = cookieparser.parse(req.headers.cookie);
+      if (!parsed.user) return;
       const user = JSON.parse(parsed.user);
       commit('user/setUser', user);
     }
